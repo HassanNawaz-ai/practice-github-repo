0a75aef51d6375ae938e78ed7d13c3982fab5227
HassanNawaz-ai <hassan.nawaz@dynamicbits.com>
2025-10-16 17:13:17 +0500

    this is login feature

--- hallo.py
+++ hallo.py
@@ -1,1 +1,8 @@
 print("hello my name is hassan nawaz")
+
+name=input("enter your name: ")
+print("hello " + name)
+password=input("enter your password: ")
+
+if password=="hassan":
+    print("welcome " + name)
